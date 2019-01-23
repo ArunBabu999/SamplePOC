74af33fc86a46ef632311c6b2ae91e24ef63d2a3
arunbabu@techjini.com <arunbabu@techjini.com>
2019-01-24 05:13:17 +0530

    Coverd most of the usecases

--- app/src/androidTest/java/example/com/samplepoc/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/example/com/samplepoc/ExampleInstrumentedTest.java
@@ -2,11 +2,22 @@ package example.com.samplepoc;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import example.com.samplepoc.model.FactsResponse;
+import example.com.samplepoc.viewmodel.FactsViewModel;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -20,7 +31,15 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("example.com.samplepoc", appContext.getPackageName());
+    }
+
+    @Test
+    public void testSucessScenario(){
+        onView(withId(R.id.recyclerview)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testErrorScenario(){
     }
 }
